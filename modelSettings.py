13fba035a769174a09083dce056290feeef748ce
Shaun Mitchell <s.mitchell1998@hotmail.co.uk>
2021-06-01 20:19:40 +0100

    Moves source files to src folder.

--- modelSettings.py
+++
@@ -1,9 +0,0 @@
-
-class ModelSettings:
-
-    def __init__(self, epochs, batchSize, bufferSize, noiseDim, numExamplesToGenerate):
-        self.epochs = epochs
-        self.batchSize = batchSize
-        self.bufferSize = bufferSize
-        self.noiseDim =  noiseDim
-        self.numExamplesToGenerate = numExamplesToGenerate
